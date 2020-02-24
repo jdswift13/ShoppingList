ea83a1a21110fddba5b285fe8a89333676a8d6f2
Jacob Swift <jdswift13@users.campus>
2020-02-24 16:31:04 -0600

    Add project files.

---
+++ ShoppingList/AddItemControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList
+{
+    /// <summary>
+    /// Interaction logic for UserControl.xaml
+    /// </summary>
+    public partial class AddItemControl: UserControl
+    {
+        public AddItemControl()
+        {
+            InitializeComponent();
+            AddItemButton.Click += OnAddItemButtonClick;
+        }
+        protected void OnAddItemButtonClick(object sender, EventArgs args)
+        {
+           
+            if (DataContext is ShoppingListData data)
+            {
+                data.Add(ItemToAdd.Text);
+                ItemToAdd.Text = "";
+            }
+            
+        }
+    }
+}
---
+++ ShoppingList/ShoppingListData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace ShoppingList
+{
+    public class ShoppingListData : INotifyPropertyChanged
+    { 
+        public event PropertyChangedEventHandler PropertyChanged;
+    
+        List<string> items = new List<string>();
+        public IEnumerable<string> Items => items;
+        public void Add(string item)
+        {
+            items.Add(item);
+        PropertyChanged?.Invoke(this, new PropertyChangingEventArgs("Items"));
+        }
+        public void Remove(string item)
+        {
+            items.Remove(item);
+        PropertyChanged?.Invoke(this, new PropertyChangingEventArgs("Items"));
+    }
+
+    }
+}
